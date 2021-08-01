04dd196fae3ecf34a51d674dab539ddaa1812d48
woojin126 <woojin126@naver.com>
2021-08-01 10:49:38 +0900

    @RestControllerAdvice exception 처리

--- src/main/java/study/RestApiCommunicate/exception/MyExceptionHandler.java
+++ src/main/java/study/RestApiCommunicate/exception/MyExceptionHandler.java
@@ -1,2 +1,15 @@
-package study.RestApiCommunicate.exception;public class MyExceptionHandler {
+package study.RestApiCommunicate.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class MyExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public String errorHandler(IllegalArgumentException e){
+        return e.getMessage();
+
+    }
 }
