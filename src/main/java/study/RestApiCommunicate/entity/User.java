51098a65348e990bdf0f61ffc139842423e0ba73
woojin126 <woojin126@naver.com>
2021-08-01 10:49:18 +0900

    User entity

--- src/main/java/study/RestApiCommunicate/entity/User.java
+++ src/main/java/study/RestApiCommunicate/entity/User.java
@@ -1,4 +1,40 @@
-package study.RestApiCommunicate.domain;
+package study.RestApiCommunicate.entity;
 
+import lombok.*;
+import study.RestApiCommunicate.domain.RequestUserDto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+@ToString
 public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+    private String phone;
+
+
+    public static User updateUser(RequestUserDto dto, User user) {
+        user.setUsername(dto.toEntity().getUsername());
+        user.setPassword(dto.toEntity().getPassword());
+        user.setPhone(dto.toEntity().getPhone());
+
+        return user;
+    }
+
+
+
+
 }
