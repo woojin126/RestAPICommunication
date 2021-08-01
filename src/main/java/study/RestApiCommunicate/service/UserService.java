dd793f02fb021751fab1ed30d2bb2c2c763ca89d
woojin126 <woojin126@naver.com>
2021-08-01 10:50:23 +0900

    Service ( update 전용)

--- src/main/java/study/RestApiCommunicate/service/UserService.java
+++ src/main/java/study/RestApiCommunicate/service/UserService.java
@@ -1,2 +1,28 @@
-package study.RestApiCommunicate.service;public class UserService {
+package study.RestApiCommunicate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.RestApiCommunicate.domain.RequestUserDto;
+import study.RestApiCommunicate.entity.User;
+import study.RestApiCommunicate.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    @Transactional
+    public User update(Long id, RequestUserDto dto){
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("값이 없습니다"));
+
+        return User.updateUser(dto, user);
+
+    }
+
+
+
 }
