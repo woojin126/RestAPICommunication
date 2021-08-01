a6c25506bf89ed47520fe2a9911f73e2ec3ab9c8
woojin126 <woojin126@naver.com>
2021-08-01 10:49:07 +0900

    응답전용 dto

--- src/main/java/study/RestApiCommunicate/domain/ResponseUserDto.java
+++ src/main/java/study/RestApiCommunicate/domain/ResponseUserDto.java
@@ -1,2 +1,22 @@
-package study.RestApiCommunicate.domain;public class ResponseUserDto {
+package study.RestApiCommunicate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ResponseUserDto<T> {
+
+    private int statusCode;
+    private T data;
+
+    public ResponseUserDto(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public ResponseUserDto(int statusCode, T data) {
+        this.statusCode = statusCode;
+        this.data = data;
+    }
 }
