1d9b9bb5ad58ea1f39421bbbe843efbfc1713268
woojin126 <woojin126@naver.com>
2021-08-01 10:48:58 +0900

    요청데이터받는 전용 DTO

--- src/main/java/study/RestApiCommunicate/domain/RequestUserDto.java
+++ src/main/java/study/RestApiCommunicate/domain/RequestUserDto.java
@@ -1,2 +1,21 @@
-package study.RestApiCommunicate.domain;public class RequestUserDto {
+package study.RestApiCommunicate.domain;
+
+import lombok.Data;
+import study.RestApiCommunicate.entity.User;
+
+@Data
+public class RequestUserDto {
+
+    private String username;
+    private String password;
+    private String phone;
+
+
+  public User toEntity(){
+        return User.builder()
+                .username(username)
+                .password(password)
+                .phone(phone)
+                .build();
+    }
 }
